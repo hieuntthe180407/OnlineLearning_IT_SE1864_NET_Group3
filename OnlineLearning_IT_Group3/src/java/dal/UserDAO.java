0fc319b36775a99dfcd82a12c6626dd5e796c9d7
AnhPQHE180725 <anhpqhe180725@fpt.edu.vn>
2024-09-17 21:26:44 +0700

    add function userDAO

--- OnlineLearning_IT_Group3/src/java/dal/UserDAO.java
+++ OnlineLearning_IT_Group3/src/java/dal/UserDAO.java
@@ -89,57 +89,54 @@ public class UserDAO extends DBContext {
     }
 
     public List<User> searchUser(String str) {
-    List<User> list = new ArrayList<>();
-    try {
-     
-        String sql = "SELECT * FROM [dbo].[User] WHERE FullName LIKE ? OR Phone LIKE ? OR Email LIKE ?";
-        PreparedStatement st = connection.prepareStatement(sql);
-        
-    
-        String searchTerm = "%" + str + "%";
-        
-       
-        st.setString(1, searchTerm);
-        st.setString(2, searchTerm);
-        st.setString(3, searchTerm);
-        
-        ResultSet rs = st.executeQuery();
-        while (rs.next()) {
-            User u = new User();
+        List<User> list = new ArrayList<>();
+        try {
+
+            String sql = "SELECT * FROM [dbo].[User] WHERE FullName LIKE ? OR Phone LIKE ? OR Email LIKE ?";
+            PreparedStatement st = connection.prepareStatement(sql);
+
+            String searchTerm = "%" + str + "%";
+
+            st.setString(1, searchTerm);
+            st.setString(2, searchTerm);
+            st.setString(3, searchTerm);
+
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                User u = new User();
                 u.setUserID(rs.getInt("userID"));
                 u.setFullName(rs.getString("fullName"));
                 u.setEmail(rs.getString("Email"));
                 u.setGender(rs.getString("Gender"));
                 u.setPassword(rs.getString("Password"));
                 u.setAddress(rs.getString("Address"));
-                 u.setPhone(rs.getString("Phone"));
+                u.setPhone(rs.getString("Phone"));
                 Role role = new Role();
                 role.setRoleId(rs.getInt("RoleID"));
                 u.setRole(role);
-            list.add(u);
+                list.add(u);
+            }
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        rs.close();
-        st.close();
-    } catch (Exception e) {
-        System.out.println(e.getMessage());
+        return list;
     }
-    return list;
-}
 
-    public List<User> filterUser(String gender, String role, String status ){
-       List<User> list = new ArrayList<>();
+    public List<User> filterUser(String gender, String role, String status) {
+        List<User> list = new ArrayList<>();
         try {
             String sql = "SELECT u.*, r.roleName FROM [dbo].[User] AS u LEFT JOIN Role AS r ON u.roleID = r.roleID WHERE Gender LIKE ? AND roleName LIKE ? AND isVerify LIKE ?";
-                  
-            PreparedStatement st = connection.prepareStatement(sql);    
-            
-            
+
+            PreparedStatement st = connection.prepareStatement(sql);
+
             st.setString(1, "%" + gender + "%");
             st.setString(2, "%" + role + "%");
             st.setString(3, "%" + status + "%");
             ResultSet rs = st.executeQuery();
-            while (rs.next()){
-               User u = new User();
+            while (rs.next()) {
+                User u = new User();
                 u.setUserID(rs.getInt("userID"));
                 u.setFullName(rs.getString("fullName"));
                 u.setEmail(rs.getString("Email"));
@@ -154,11 +151,41 @@ public class UserDAO extends DBContext {
                 list.add(u);
             }
             rs.close();
-            st.close();            
-        }
-        catch (Exception e){
+            st.close();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return list;
+    }
+
+    public User getUserProfilebyId(int id) {
+        String sql = "Select u.[UserID], u.[FullName], u.[DateOfBirth], u.[Email], u.[Password], u.[Phone], u.[Address],u.[Gender], r.[RoleName], u.[Avatar]\n"
+                + "from [dbo].[User] u, [dbo].[Role] r\n"
+                + "where r.RoleID = u.RoleID AND u.[UserID] = ?;";
+        User u = new User();
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+
+                u.setUserID(rs.getInt("userID"));
+                u.setFullName(rs.getString("fullName"));
+                u.setDateOfBirth(rs.getString("DateOfBirth"));
+                u.setEmail(rs.getString("Email"));
+                u.setPassword(rs.getString("Password"));
+                u.setGender(rs.getString("Gender"));
+                u.setPhone(rs.getString("Phone"));
+                u.setAddress(rs.getString("Address"));
+                Role role = new Role();
+                role.setRoleName(rs.getString("RoleName"));
+                u.setRole(role);
+                u.setAvatar(rs.getString("Avatar"));
+
+            }
+        } catch (Exception e) {
+        }
+        return u;
+
     }
 }
