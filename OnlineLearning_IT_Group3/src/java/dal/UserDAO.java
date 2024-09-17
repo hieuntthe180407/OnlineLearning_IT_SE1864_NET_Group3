32e59443cb41c39c32b8e2f93a876d1471ba39d5
AnhPQHE180725 <anhpqhe180725@fpt.edu.vn>
2024-09-17 21:22:41 +0700

    fix conflictscode

--- OnlineLearning_IT_Group3/src/java/dal/UserDAO.java
+++ OnlineLearning_IT_Group3/src/java/dal/UserDAO.java
@@ -88,75 +88,79 @@ public class UserDAO extends DBContext {
         }
     }
 
-//    public List<User> searchUser(String str) {
-//    List<User> list = new ArrayList<>();
-//    try {
-//     
-//        String sql = "SELECT * FROM [dbo].[User] WHERE FullName LIKE ? OR Phone LIKE ? OR Email LIKE ?";
-//        PreparedStatement st = connection.prepareStatement(sql);
-//        
-//    
-//        String searchTerm = "%" + str + "%";
-//        
-//       
-//        st.setString(1, searchTerm);
-//        st.setString(2, searchTerm);
-//        st.setString(3, searchTerm);
-//        
-//        ResultSet rs = st.executeQuery();
-//        while (rs.next()) {
-//            User u = new User();
-//                u.setUserID(rs.getInt("userID"));
-//                u.setFullName(rs.getString("fullName"));
-//                u.setEmail(rs.getString("Email"));
-//                u.setGender(rs.getString("Gender"));
-//                u.setPassword(rs.getString("Password"));
-//                u.setAddress(rs.getString("Address"));
-//                 u.setPhone(rs.getString("Phone"));
-//                u.setRoleID(rs.getInt("RoleID"));
-//            list.add(u);
-//        }
-//        rs.close();
-//        st.close();
-//    } catch (Exception e) {
-//        System.out.println(e.getMessage());
-//    }
-//    return list;
-//}
-//
-//    public List<User> filterUser(String gender, String role, String status ){
-//       List<User> list = new ArrayList<>();
-//        try {
-//            String sql = "SELECT u.*, r.roleName FROM [dbo].[User] AS u LEFT JOIN Role AS r ON u.roleID = r.roleID WHERE Gender LIKE ? AND roleName LIKE ? AND isVerify LIKE ?";
-//                  
-//            PreparedStatement st = connection.prepareStatement(sql);    
-//            
-//            
-//            st.setString(1, "%" + gender + "%");
-//            st.setString(2, "%" + role + "%");
-//            st.setString(3, "%" + status + "%");
-//            ResultSet rs = st.executeQuery();
-//            while (rs.next()){
-//               User u = new User();
-//                u.setUserID(rs.getInt("userID"));
-//                u.setFullName(rs.getString("fullName"));
-//                u.setEmail(rs.getString("Email"));
-//                u.setPassword(rs.getString("Password"));
-//                u.setAddress(rs.getString("Address"));
-//                u.setGender(rs.getString("Gender"));
-//                u.setPhone(rs.getString("Phone"));
-//                u.setRoleName(rs.getString("RoleName"));
-//                u.setRoleID(rs.getInt("RoleID"));
-//                list.add(u);
-//            }
-//            rs.close();
-//            st.close();            
-//        }
-//        catch (Exception e){
-//            System.out.println(e.getMessage());
-//        }
-//        return list;
-//    }
+    public List<User> searchUser(String str) {
+    List<User> list = new ArrayList<>();
+    try {
+     
+        String sql = "SELECT * FROM [dbo].[User] WHERE FullName LIKE ? OR Phone LIKE ? OR Email LIKE ?";
+        PreparedStatement st = connection.prepareStatement(sql);
+        
+    
+        String searchTerm = "%" + str + "%";
+        
+       
+        st.setString(1, searchTerm);
+        st.setString(2, searchTerm);
+        st.setString(3, searchTerm);
+        
+        ResultSet rs = st.executeQuery();
+        while (rs.next()) {
+            User u = new User();
+                u.setUserID(rs.getInt("userID"));
+                u.setFullName(rs.getString("fullName"));
+                u.setEmail(rs.getString("Email"));
+                u.setGender(rs.getString("Gender"));
+                u.setPassword(rs.getString("Password"));
+                u.setAddress(rs.getString("Address"));
+                 u.setPhone(rs.getString("Phone"));
+                Role role = new Role();
+                role.setRoleId(rs.getInt("RoleID"));
+                u.setRole(role);
+            list.add(u);
+        }
+        rs.close();
+        st.close();
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+    return list;
+}
+
+    public List<User> filterUser(String gender, String role, String status ){
+       List<User> list = new ArrayList<>();
+        try {
+            String sql = "SELECT u.*, r.roleName FROM [dbo].[User] AS u LEFT JOIN Role AS r ON u.roleID = r.roleID WHERE Gender LIKE ? AND roleName LIKE ? AND isVerify LIKE ?";
+                  
+            PreparedStatement st = connection.prepareStatement(sql);    
+            
+            
+            st.setString(1, "%" + gender + "%");
+            st.setString(2, "%" + role + "%");
+            st.setString(3, "%" + status + "%");
+            ResultSet rs = st.executeQuery();
+            while (rs.next()){
+               User u = new User();
+                u.setUserID(rs.getInt("userID"));
+                u.setFullName(rs.getString("fullName"));
+                u.setEmail(rs.getString("Email"));
+                u.setPassword(rs.getString("Password"));
+                u.setAddress(rs.getString("Address"));
+                u.setGender(rs.getString("Gender"));
+                u.setPhone(rs.getString("Phone"));
+                Role r = new Role();
+                r.setRoleId(rs.getInt("RoleId"));
+                r.setRoleName(rs.getString("RoleName"));
+                u.setRole(r);
+                list.add(u);
+            }
+            rs.close();
+            st.close();            
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
     public User getUserProfilebyId(int id) {
         String sql = "Select u.[UserID], u.[FullName], u.[DateOfBirth], u.[Email], u.[Password], u.[Phone], u.[Address],u.[Gender], r.[RoleName], u.[Avatar]\n"
                 + "from [dbo].[User] u, [dbo].[Role] r\n"
