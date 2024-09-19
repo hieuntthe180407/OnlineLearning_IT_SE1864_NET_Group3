65238f210af00c0ff717da47d06fb5ff2284efac
AnhLDHE180218 <anhldhe180218@fpt.edu.vn>
2024-09-19 20:58:19 +0700

    insert getHostPost

--- OnlineLearning_IT_Group3/src/java/dal/BlogDAO.java
+++ OnlineLearning_IT_Group3/src/java/dal/BlogDAO.java
@@ -4,10 +4,42 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Blog;
+
 /**
  *
  * @author ADMIN
  */
-public class BlogDAO {
+public class BlogDAO extends DBContext{
+    public List<Blog> getHostPost() {
+        List<Blog> blogs = new ArrayList<>();
+        try {
+            String sql = "SELECT TOP 6 * FROM Blogs where status='Published'";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Blog blog = new Blog(
+                    rs.getInt("BlogId"),
+                    rs.getInt("UserId"),
+                    rs.getInt("CategoryId"),
+                    rs.getString("Title"),
+                    rs.getString("Content"),
+                    rs.getString("Status"),
+                    rs.getString("FeaturedImage"),
+                    rs.getTimestamp("CreatedAt"),
+                    rs.getTimestamp("UpdatedAt")
+                );
+                blogs.add(blog);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: "  + e);
+        }
+        return blogs;
+    }
     
 }
