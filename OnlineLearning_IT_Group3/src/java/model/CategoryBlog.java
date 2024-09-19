96fbc715e25265985080723412627e78d649a1a7
AnhLDHE180218 <anhldhe180218@fpt.edu.vn>
2024-09-19 21:11:41 +0700

    Update CategoryBlog.java

--- OnlineLearning_IT_Group3/src/java/model/CategoryBlog.java
+++ OnlineLearning_IT_Group3/src/java/model/CategoryBlog.java
@@ -4,10 +4,58 @@
  */
 package model;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author ADMIN
  */
 public class CategoryBlog {
+    private int categoryId;
+    private String categoryName;
+    private String description;
+    private Timestamp createdAt;
+
+    public CategoryBlog() {
+    }
+
+    public CategoryBlog(int categoryId, String categoryName, String description, Timestamp createdAt) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
     
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
